56fee410c47ba50c5d85bd6838d631bb32976c0f
Oguz Meteer <info@guztech.nl>
2020-01-06 22:35:33 +0100

    Implement toplevel Component for the Lattice HX8K board
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- src/main/scala/shdl6800/cpu_lattice_ice40hx8k.scala
+++ src/main/scala/shdl6800/cpu_lattice_ice40hx8k.scala
@@ -3,6 +3,8 @@ package shdl6800
 import lattice._
 import spinal.core._
 
+import scala.collection.mutable
+
 // This is the software ROM
 object mem {
   val mem = Map(
@@ -14,31 +16,98 @@ object mem {
   )
 }
 
-class SHDL6800 extends Component {
+class SHDL6800(val defaultClkFreq: HertzNumber,
+               val slowClock: Boolean = false,
+               val fakeMem: Boolean = false) extends Component
+{
   val io = new Bundle {
-    val clk    = in Bool
+    val clk1   = in Bool
+    val clk2   = in Bool
     val resetn = in Bool
+    val addr   = out Bits(16 bits)
+    val rw     = out Bits(1 bit)
+
+    val led  =  fakeMem generate(out Bits(8 bits))
+    val data = !fakeMem generate(inout(Analog(Bits(8 bits))))
   }
 
-  val cpu = new Core()
+  // Define clock domains
   val ph1 = ClockDomain.internal(
     name   = "ph1",
     config = ClockDomainConfig(
       clockEdge        = RISING,
       resetKind        = SYNC,
-      resetActiveLevel = LOW))
+      resetActiveLevel = HIGH))
+
   val ph2 = ClockDomain.internal(
     name = "ph2",
     config = ClockDomainConfig(
       clockEdge        = FALLING,
       resetKind        = SYNC,
-      resetActiveLevel = LOW))
+      resetActiveLevel = HIGH))
 
+  if(slowClock) {
+    val clk_freq = defaultClkFreq
+    val timer = Reg(UInt(log2Up((clk_freq / 2).toInt) bits)) init(((clk_freq / 2) - 1).toInt)
+    val tick  = Reg(Bool) init(False)
+
+    when(timer === 0) {
+      timer := (((clk_freq / 2) - 1).toInt)
+      tick := ~tick
+    } otherwise {
+      timer := (timer - 1)
+    }
+
+    // Hook up clocks and reset to pins
+    ph1.clock := tick
+    ph1.reset := io.resetn
+    ph2.clock := ~tick
+    ph2.reset := io.resetn
+  } else {
+    // Hook up clocks and reset to pins
+    ph1.clock := io.clk1
+    ph1.reset := io.resetn
+    ph2.clock := io.clk2
+    ph2.reset := io.resetn
+  }
+
+  val CoreArea = new ClockingArea(ph1) {
+    val cpu = new Core()
+
+    // Hook up address lines to pins
+    io.addr := cpu.io.Addr
+
+    if(fakeMem) {
+      switch(cpu.io.Addr) {
+        mem.mem.foreach{ case (addr, data) => is(addr) {cpu.io.Din := data} }
+        default {cpu.io.Din := 0xFF}
+      }
+      io.led(io.led.getWidth - 1 downto 0) := cpu.io.Addr(io.led.getWidth - 1 downto 0)
+    } else {
+      // Hook up data in/out + direction to pins
+      cpu.io.Din := io.data
+
+      when(cpu.io.RW === 0) {
+        io.data := cpu.io.Dout
+      }
+    }
+
+    io.rw := cpu.io.RW
+  }
 }
 
 object cpu_lattice_ice40hx8k {
   def main(args: Array[String]): Unit = {
-    val toplevelName = SpinalVerilog(new SHDL6800()).toplevelName
+    val design          = SpinalVerilog(new SHDL6800(defaultClkFreq = 12 MHz, slowClock = true, fakeMem = true))
+    val toplevelName    = design.toplevelName
+    val toplevelSignals = design.toplevel.getAllIo
+    toplevelSignals.foreach{ case s => println(s.toString()) }
+
+//    val pinMapping      = toplevelSignals.toMap(
+//      "io_clk1"   -> "J3",
+//      "io_clk2"   -> "G1",
+//      "io_resetn" -> "R9"
+//    )
 
     val report = YosysNextpnrFlow(
       toplevelName = toplevelName,
